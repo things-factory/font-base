16c44cd089674b2a05f60ae2ee4e656e0ae1d9e4
Hearty Oh <heartyoh@hatiolab.com>
2019-10-08 11:52:10 +0800

    v0.1.4

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,8 +1,13 @@
 import { store } from '@things-factory/shell'
 import font from './reducers/font'
+import { auth } from '@things-factory/auth-base'
 
 export default function bootstrap() {
   store.addReducers({
     font
+  })
+
+  auth.on('signin', () => {
+    store.dispatch(fetchFontList({}))
   })
 }
