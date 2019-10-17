df79facc06dd721b8c3fba3186b59bebf4de67cf
lllleeee <lcj1123@hatiolab.com>
2019-10-17 21:01:31 +0900

    구글 폰트 리스트 가져오는 서비스 추가
    #2

---
+++ server/controllers/google-fonts.ts
@@ -0,0 +1,14 @@
+import { config } from '@things-factory/env'
+import { fetch } from 'apollo-env'
+
+const address = 'https://www.googleapis.com/webfonts/v1/webfonts?key='
+
+export const getAllGoogleFonts = async () => {
+  var key = config.get('googleFontAPIKey')
+  var response = await fetch(`${address}${key}`)
+  if (response.ok) {
+    return (await response.json()).items.map(font => font.family)
+  } else {
+    return []
+  }
+}
