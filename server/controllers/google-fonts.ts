876a1dafb257c7233be9dd205f93f461731231b6
lllleeee <lcj1123@hatiolab.com>
2019-10-18 19:40:16 +0900

    Update google-fonts.ts

--- server/controllers/google-fonts.ts
+++ server/controllers/google-fonts.ts
@@ -3,7 +3,7 @@ import { fetch } from 'apollo-env'
 
 const address = 'https://www.googleapis.com/webfonts/v1/webfonts?key='
 
-export const getAllGoogleFonts = async () => {
+export async function getAllGoogleFonts() {
   var key = config.get('googleFontAPIKey')
   var response = await fetch(`${address}${key}`)
   if (response.ok) {
