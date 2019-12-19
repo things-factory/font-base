ad6ed84a27ed37cd067124c7ac8c1d0947239a35
Hearty Oh <heartyoh@hatiolab.com>
2019-12-19 17:52:20 +0800

    remove auth event handler

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,14 +1,11 @@
 import { store } from '@things-factory/shell'
 import { fetchFontList } from './actions/font'
 import font from './reducers/font'
-import { auth } from '@things-factory/auth-base'
 
 export default function bootstrap() {
   store.addReducers({
     font
   })
 
-  auth.on('signin', () => {
-    store.dispatch(fetchFontList())
-  })
+  store.dispatch(fetchFontList())
 }
