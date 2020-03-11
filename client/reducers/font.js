386099411148d82777194f815e76958e4a653d54
lllleeee <33271505+lllleeee@users.noreply.github.com>
2020-03-11 18:43:43 +0900

    로컬 폰트 사용 기능 추가
    #4

--- client/reducers/font.js
+++ client/reducers/font.js
@@ -6,14 +6,48 @@ const font = (state = [], action) => {
     case UPDATE_FONT_LIST:
       let newState = action.list
 
-      let googles = newState.filter(font => font.provider === 'google')
-      if (googles.length > 0) {
-        WebFont.load({
-          google: {
-            families: googles.map(font => font.name)
-          }
-        })
+      let googles = []
+      let customs = []
+      let customFontCSS = ''
+
+      newState.forEach(font => {
+        switch (font.provider) {
+          case 'google':
+            googles.push(font.name)
+            break
+          case 'custom':
+            customs.push(font.name)
+            customFontCSS += `@font-face {
+  font-family: '${font.name}';
+  src: local('${font.name}')${font.uri ? `, url(${font.uri})` : ''};
+}
+` // TODO: format('woff2') 등 파일 형식에 맞는 포맷 추가
+            break
+        }
+      })
+
+      let style = document.head.querySelector('#custom-fonts')
+      if (!style) {
+        style = document.createElement('style')
+        style.id = 'custom-fonts'
+        style.type = 'text/css'
+        document.head.appendChild(style)
       }
+      style.innerHTML = customFontCSS
+
+      // TODO: typekit 등 타 서비스 지원
+      let WebFontConfig = {}
+      if (googles.length) {
+        WebFontConfig.google = {
+          families: googles
+        }
+      }
+      if (customs.length) {
+        WebFontConfig.custom = {
+          families: customs
+        }
+      }
+      if (Object.keys(WebFontConfig).length) WebFont.load(WebFontConfig)
 
       return newState
 
